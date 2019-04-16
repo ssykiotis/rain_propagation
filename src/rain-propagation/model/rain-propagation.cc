e1f523d4aafc464a18c7797a2a179a2b3a4ab1ba
Stavros Sykiotis <stasykiotis@gmail.com>
2019-04-16 21:04:30 +0200

    first commit

---
+++ src/rain-propagation/model/rain-propagation.cc
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "rain-propagation.h"
+
+namespace ns3 {
+
+/* ... */
+
+
+}
+
---
+++ src/rain-propagation/model/rain-propagation.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef RAIN_PROPAGATION_H
+#define RAIN_PROPAGATION_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* RAIN_PROPAGATION_H */
+
